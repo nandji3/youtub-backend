260d2bb944ca490e18c776d9dc18007345de8e8a
nandji3@gmail.com <nandji3@gmail.com>
2025-06-17 15:55:59 +0530

    production level backend complete setup done with cloudinary and multer for file uploading

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, 'public/temp')) // safer path resolution
+    },
+    filename: function (req, file, cb) {
+        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        // cb(null, file.originalname + '-' + uniqueSuffix)
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+module.exports = upload;
